2ead94e9ea87b483d22157335d63f533193586ee
manojbarkicodee <manojjunjappabarki@gmail.com>
2022-06-17 16:27:45 +0530

    comit

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from "react-router-dom";
 import { store } from '../src/store/store';
+import { ChakraProvider } from '@chakra-ui/react'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
     <BrowserRouter>
+    <ChakraProvider>
       <App />
+      </ChakraProvider>
     </BrowserRouter>
     </Provider>
   </React.StrictMode>
