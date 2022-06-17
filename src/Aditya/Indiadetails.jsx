16733fc1db5ab51850f14436af118109ea31cee8
adityamuthal22 <adityamuthal.2208@gmail.com>
2022-06-17 14:44:56 +0530

    Aditya code added

---
+++ src/Aditya/Indiadetails.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import styles from './mystyle.module.css'; 
+
+const Indiadetails = () => {
+   let details= JSON.parse(localStorage.getItem("adidetails"))
+   let {id}=useParams()
+//    console.log(details);
+   let showdata=details.filter((el,index)=>{
+    return index===Number(id)
+   })
+   console.log(showdata[0])
+//    console.log(id)
+  return (
+    <div className={styles.indiadetailsadi}>
+        <h1 className={styles.h1details}>{showdata[0].title}</h1>
+        <h3 className={styles.descdetails}>{showdata[0].description}</h3>
+        <img className={styles.imgdetails} src={showdata[0].urlToImage} alt="" />
+        <p className={styles.paradetails}>{showdata[0].content}</p>
+    </div>
+  )
+}
+
+export default Indiadetails
